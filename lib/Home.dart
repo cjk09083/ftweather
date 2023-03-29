78900645f56ce06c6243f75b538cbbe690161937
cjk09083 <cjk09083@naver.com>
2023-03-29 17:10:17 +0900

    add naver map

--- lib/Home.dart
+++ lib/Home.dart
@@ -5,28 +5,35 @@ import 'package:naver_map_plugin/naver_map_plugin.dart';
 import 'MyModel.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
-  final CameraPosition _mapPosition = const CameraPosition(
-    target: LatLng(37.5666805, 126.9784147),
-    zoom: 15,
-  );
-
+  // super.key를 전달하여 Key를 받을 수 있는 선택적 인자를 추가
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // mapPosition과 markers를 MyModel에서 가져옴
+    final mapPosition = Provider.of<MyModel>(context).mapPosition;
+    final model = Provider.of<MyModel>(context, listen: false);
+    final markers = Provider.of<MyModel>(context).markers;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Weather App'),
       ),
       body: Center(
         child: NaverMap(
+          // NaverMapController를 model에 전달
+          onMapCreated: (controller) {
+            model.setController(controller);
+          },
           mapType: MapType.Hybrid,
-          initialCameraPosition: _mapPosition,
+          initialCameraPosition: mapPosition, // MyModel에서 가져온 mapPosition 사용
+          // initLocationTrackingMode: LocationTrackingMode.Follow, // 권한 획득 시 현재 위치를 따라가는 모드 설정
+          markers: markers, // MyModel에서 가져온 markers 사용
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          // addMarker() 메서드를 호출하여 현재 위치에 마커 추가
           Provider.of<MyModel>(context, listen: false).addMarker();
         },
         tooltip: 'Increment',
--- lib/MyModel.dart
+++ lib/MyModel.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/foundation.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
+import 'dart:developer';
 
 class MyModel extends ChangeNotifier {
-  int _count = 0;
+  final TAG = "ftweather"; // 로그 태그
 
-  int get count => _count;
+  // 초기 카메라 위치 설정
+  final CameraPosition _mapPosition = const CameraPosition(
+    target: LatLng(37.5666805, 126.9784147),
+    zoom: 15,
+  );
 
-  void addMarker() {
-    _count++;
+  // 카메라 위치를 가져오는 getter
+  CameraPosition get mapPosition => _mapPosition;
+
+  // NaverMapController 변수
+  NaverMapController? _controller;
+
+  // 마커 목록 관리
+  final List<Marker> _markers = [];
+  List<Marker> get markers => _markers;
+
+  // NaverMapController 초기화 메서드
+  void setController(NaverMapController controller) {
+    _controller = controller;
+  }
+
+  // 현재 위치에 마커를 추가하는 메서드
+  Future<void> addMarker() async {
+    // 로그 출력
+    log("$TAG : addMarker");
+
+    // NaverMapController가 초기화되었는지 확인
+    if (_controller != null) {
+      // 현재 카메라 위치 가져오기
+      CameraPosition currentCameraPosition = await _controller!.getCameraPosition();
+      // 로그 출력
+      log("$TAG : Pos $currentCameraPosition");
+
+      // 현재 위치에 마커 추가
+      final marker = Marker(
+        markerId: _markers.length.toString(),
+        position: currentCameraPosition.target,
+      );
+
+      // 마커 목록에 추가
+      _markers.add(marker);
+    }
+
+    // 마커가 추가되었음을 알림
     notifyListeners();
   }
 }
